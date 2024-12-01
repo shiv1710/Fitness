bdb4076035c0f47d5d0b71263c27483dbe1f86de
Shiv Patel <shivpatel17010@gmail.com>
2024-12-02 01:56:37 +0530

    Home and Progress

---
+++ FitnessTracker/FitnessTabView.swift
@@ -0,0 +1,42 @@
+//
+//  FitnessTabView.swift
+//  FitnessTracker
+//
+//  Created by Shiv Patel on 02/12/24.
+//
+
+import SwiftUI
+
+struct FitnessTabView: View {
+    @State var selectedTab = "Home"
+    
+    init(){
+        let appearance  = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.stackedLayoutAppearance.selected.iconColor = .green
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.green]
+        
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    var body: some View {
+        TabView(selection: $selectedTab){
+            HomeView()
+                .tag("Home")
+                .tabItem{
+                    Image(systemName: "house")
+                }
+            HistoricView()
+                .tag("Historic")
+                .tabItem{
+                    Image(systemName: "chart.xyaxis.line" )
+                    
+                    Text("Charts")
+                }
+        }
+    }
+}
+
+#Preview {
+    FitnessTabView()
+}
---
+++ FitnessTracker/HistoricView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricView.swift
+//  FitnessTracker
+//
+//  Created by Shiv Patel on 02/12/24.
+//
+
+import SwiftUI
+
+struct HistoricView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricView()
+}
