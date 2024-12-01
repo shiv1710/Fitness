e3f6f2305021a274331fa81a151f8d9cec532713
Shiv Patel <shivpatel17010@gmail.com>
2024-12-02 00:45:11 +0530

    Initial Commit

---
+++ FitnessTracker/FitnessTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerApp.swift
+//  FitnessTracker
+//
+//  Created by Shiv Patel on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
