251992697ce7c52f815c5d3d5e059030cb38dcb3
Shiv Patel <shivpatel17010@gmail.com>
2024-12-02 00:54:21 +0530

    first

--- FitnessTracker/ContentView.swift
+++ FitnessTracker/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Shiv!")
+            Text("Shi!")
         }
         .padding()
     }
